fcd2f45723ca5a0ed3c491d6b38a977face49c4d
Fabiano Chavier <chaviersantos70@gmail.com>
2025-12-30 20:16:57 -0300

    Revert "frt"
    This reverts commit b5342d9116fd40ae5617b55c77a415bf74f3e5da.

---
+++ src/test/java/com/chavier/chavierWebMidia/ChavierWebMidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chavier.chavierWebMidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavierWebMidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
